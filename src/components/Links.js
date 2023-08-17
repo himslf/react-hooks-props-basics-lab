a29d642f8d1923e034ca35934b4fc73ef0e47189
JASON YOUNG <moorslf@gmail.com>
2023-08-17 07:51:56 -0400

    PropsBasicsLab

---
+++ src/components/Links.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+function linkS(props){
+     return(
+     <div>
+         <h3>Links</h3>
+         <a href={props.github}>{props.github}</a>
+         <a href={props.linkedin}>{props.linkedin}</a>
+     </div>
+
+     )
+}
+
+export default linkS;
